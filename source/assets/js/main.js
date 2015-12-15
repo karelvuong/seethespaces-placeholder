44d2084530ace2b8a22ef405aa8e6360bad2068a
Karel Vuong <vuongkarel@gmail.com>
2015-12-15 10:38:58 -0500

    Updates

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -5,50 +5,23 @@
 //= require_tree
 
 document.addEventListener("DOMContentLoaded", function(e) {
-  // Toggle sidebar
   (function() {
-    var toggle = document.getElementsByClassName('js-toggle-sidebar'),
-        toggleCount = toggle.length,
-        sidebar = document.getElementsByClassName('site-sidebar')[0];
-
-    for (var i = 0 ; i < toggleCount ; i++) {
-      toggle[i].onclick = function() {
-        toggleSidebarClass();
+    $(window).scroll(function () {
+      if ($('.header--space').length) {
+        if ( $(window).scrollTop() > $('.header--space').offset().top) {
+          $('.header--space').addClass('header--space--fixed');
+        } else {
+          $('.header--space').removeClass('header--space--fixed');
+        }
       }
-    }
+    });
 
-    function toggleSidebarClass() {
-      var toggled = false;
-
-      if (sidebar.classList)
-        toggled = sidebar.classList.contains("active");
-      else
-        toggled = new RegExp('(^| )active( |$)', 'gi').test(sidebar.className);
-
-      if (toggled) {
-        if (sidebar.classList)
-          sidebar.classList.remove("active");
-        else
-          sidebar.className = sidebar.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-
-        document.getElementById('toggleSidebar').dataset.glyph = "menu";
-      } else {
-        if (sidebar.classList)
-          sidebar.classList.add("active");
-        else
-          sidebar.className += " active";
-
-        document.getElementById('toggleSidebar').dataset.glyph = "x";
-      }
-    }
-
-    $('.js-fb-popup').click(function(event) {
+    $('.js-share-facebook').click(function(event) {
       var width  = 575,
           height = 400,
           left   = ($(window).width()  - width)  / 2,
           top    = ($(window).height() - height) / 2,
           data   = {
-            "url": encodeURIComponent($(this).data("facebook-url"))
           },
           url    = "https://www.facebook.com/sharer/sharer.php?u=" + data.url,
           opts   = 'status=1' +
@@ -57,23 +30,20 @@ document.addEventListener("DOMContentLoaded", function(e) {
                    ',top='    + top    +
                    ',left='   + left;
 
-      window.open(url, 'facebook-share-dialog', opts);
+      window.open(url, 'facebook', opts);
 
       return false;
     });
 
-    $('.js-tweet-popup').click(function(event) {
+    $('.js-share-twitter').click(function(event) {
       var width  = 626,
           height = 436,
           left   = ($(window).width()  - width)  / 2,
           top    = ($(window).height() - height) / 2,
           data   = {
-            "text": encodeURIComponent($(this).data("twitter-text")),
-            "url": encodeURIComponent($(this).data("twitter-url")),
-            "via": encodeURIComponent($(this).data("twitter-via")),
-            "reply": encodeURIComponent($(this).data("twitter-in-reply-to"))
+            "text": encodeURIComponent($(this).data("share-text"))
           },
-          url    = "https://twitter.com/intent/tweet?text=" + data.text + "&url=" + data.url + "&related=" + data.via + "&in-reply-to=" + data.reply,
+          url    = "https://twitter.com/intent/tweet?text=" + data.text + "&url=" + data.url,
           opts   = 'status=1' +
                    ',width='  + width  +
                    ',height=' + height +
@@ -83,16 +53,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
       window.open(url, 'twitter', opts);
 
       return false;
-    });
-  })();
-
-  // Run Moment to convert absolute times to relative
-  (function () {
-    var elTimeFromNow = document.getElementsByClassName("js-time-fromNow");
-
-    [].forEach.call(elTimeFromNow, function(el) {
-      var time = moment(el.dataset.time, "YYYY-MM-DD").fromNow();
-      el.innerHTML = time;
     });
   })();
 });
