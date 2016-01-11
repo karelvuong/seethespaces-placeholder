63874b6f2e15f187e2a288ad98d6a6029e9c8e90
Karel Vuong <vuongkarel@gmail.com>
2016-01-11 08:55:27 -0500

    Add regions to spaces + index page

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,19 +1,44 @@
 //= require lib/bootstrap.min.js
 //= require lib/moment.min.js
-//= require lib/widow.min.js
+//= require lib/jquery.scrolltofixed.min.js
 //= require lib/zoom.min.js
 //= require_tree
 
 document.addEventListener("DOMContentLoaded", function(e) {
   (function() {
+    // Fix header
     $(window).scroll(function () {
       if ($('.header--space').length) {
         if ( $(window).scrollTop() > $('.header--space').offset().top) {
-          $('.header--space').addClass('header--space--fixed');
+          $('.header--space').addClass('header--fixed');
         } else {
-          $('.header--space').removeClass('header--space--fixed');
+          $('.header--space').removeClass('header--fixed');
         }
       }
+    });
+
+
+    // Dock each letter as it arrives just below the docked header, pushing the
+    // previous letter up the page.
+    var letters = $('.block-list-letter-text');
+    letters.each(function(i) {
+      var letter = $(letters[i]);
+      var next = letters[i + 1];
+
+      letter.scrollToFixed({
+        minWidth: 768,
+        marginTop: 6,
+        limit: function() {
+          var limit = 0;
+          if (next) {
+            limit = $(next).offset().top - $(this).outerHeight(true) - 85;
+          } else {
+            limit = $(this).outerHeight(true) - 85;
+          }
+          return limit;
+        },
+        zIndex: 999
+      });
     });
 
     $('.js-share-facebook').click(function(event) {
@@ -23,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
           top    = ($(window).height() - height) / 2,
           data   = {
           },
-          url    = "https://www.facebook.com/sharer/sharer.php?u=" + data.url,
+          url    = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href,
           opts   = 'status=1' +
                    ',width='  + width  +
                    ',height=' + height +
@@ -43,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
           data   = {
             "text": encodeURIComponent($(this).data("share-text"))
           },
-          url    = "https://twitter.com/intent/tweet?text=" + data.text + "&url=" + data.url,
+          url    = "https://twitter.com/intent/tweet?text=" + data.text + "&url=" + window.location.href,
           opts   = 'status=1' +
                    ',width='  + width  +
                    ',height=' + height +
